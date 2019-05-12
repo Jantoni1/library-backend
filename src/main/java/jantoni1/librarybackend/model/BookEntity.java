1fa8c40561c4812dc24483d0d120a7875afdce07
Jakub Antoniak <jantoni1@mion.elka.pw.edu.pl>
2019-05-12 02:15:42 +0200

    fixing bugs

--- src/main/java/jantoni1/librarybackend/model/BookEntity.java
+++ src/main/java/jantoni1/librarybackend/model/BookEntity.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Getter
 @Setter
 @Table(name = "books")
+@Entity
 public class BookEntity {
 
     @Id
--- src/main/java/jantoni1/librarybackend/repository/BookRepository.java
+++ src/main/java/jantoni1/librarybackend/repository/BookRepository.java
@@ -2,9 +2,11 @@ package jantoni1.librarybackend.repository;
 
 import jantoni1.librarybackend.model.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface BookRepository extends JpaRepository<BookEntity, Integer> {
 
     Optional<BookEntity> findByIsbn(String isbn);
